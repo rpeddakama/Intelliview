539cfa19c31e6283c992612432b36046d7fc9458
rpeddakama <rishipeddakama@gmail.com>
2024-06-26 19:22:48 -0700

    remove uneeded files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import MainSection from "./components/MainSection";
-import Settings from "./components/Settings";
-import About from "./components/About";
-import Services from "./components/Services";
-import Contact from "./components/Contact";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import ProtectedRoutes from "./context/ProtectedRoutes";
-import HelloWorld from "./components/HelloWorld";
 import Profile from "./components/Profile";
 import Upload from "./components/Upload";
 import Recorder from "./components/Recorder";
@@ -19,19 +13,12 @@ import TempForm from "./components/tempForm";
 const App: React.FC = () => {
   return (
     <Router>
-      {/* <Header /> */}
       <Routes>
-        <Route path="/" element={<MainSection />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="about" element={<About />} />
-        <Route path="services" element={<Services />} />
-        <Route path="contact" element={<Contact />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="upload" element={<Upload />} />
         <Route path="tempform" element={<TempForm />} />
         <Route element={<ProtectedRoutes />}>
-          <Route path="/hello-world" element={<HelloWorld />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="profile" element={<Profile />} />
           <Route path="recorder" element={<Recorder />} />
