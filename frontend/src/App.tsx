01ace678e01ded3d398c4eb5fd6f32b3d0b83cdc
rpeddakama <rishipeddakama@gmail.com>
2024-07-01 17:49:42 -0700

    global theme features

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+
+import theme from "./theme/theme";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -11,25 +14,29 @@ import TempForm from "./components/tempForm";
 import TempSessions from "./components/TempSessions";
 import SessionDetail from "./components/SessionDetail";
 import SignUpForm from "./components/helloworld";
+import { CssBaseline } from "@mui/material";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="helloworld" element={<SignUpForm />} />
-        <Route path="register" element={<Register />} />
-        <Route path="tempform" element={<TempForm />} />
-        <Route element={<ProtectedRoutes />}>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="recorder" element={<Recorder />} />
-          <Route path="past-sessions" element={<PastSessions />} />
-          <Route path="temp-sessions" element={<TempSessions />} />
-          <Route path="/session/:id" element={<SessionDetail />} />
-        </Route>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="helloworld" element={<SignUpForm />} />
+          <Route path="register" element={<Register />} />
+          <Route path="tempform" element={<TempForm />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="recorder" element={<Recorder />} />
+            <Route path="past-sessions" element={<PastSessions />} />
+            <Route path="temp-sessions" element={<TempSessions />} />
+            <Route path="/session/:id" element={<SessionDetail />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
